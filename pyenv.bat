512f246f8de7cd5f5a228f71eb50ff491e62dfd4
elmotec <jlecomte1972@yahoo.com>
2012-06-03 19:29:22 -0400

    bumped version number to 1.0. Now handles wildcard when removing paths

--- pyenv.bat
+++ pyenv.bat
@@ -3,8 +3,8 @@
 @rem remove any PYTHONHOME setting.
 set PYTHONHOME=
 
-if "%1" == "3" call uniquepath --remove C:\Python27 --remove C:\Python27\scripts --append C:\Python32 --append C:\Python32\scripts PATH
-if "%1" == "2" call uniquepath --remove C:\Python32 --remove C:\Python32\scripts --append C:\Python27 --append C:\Python27\scripts PATH
+if "%1" == "3" call uniquepath --remove *Python* --append C:\Python32 --append C:\Python32\scripts PATH
+if "%1" == "2" call uniquepath --remove *Python* --append C:\Python27 --append C:\Python27\scripts PATH
 
 exit /B %ERRORLEVEL%
 
