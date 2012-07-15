91420befb6310a803835b2956b16e6b412d2caf0
elmotec <jlecomte1972@yahoo.com>
2012-07-15 13:51:27 -0400

    added handling of -d --debug option to not try to execute the output

--- uniquepath.bat
+++ uniquepath.bat
@@ -1,15 +1,18 @@
 @echo off
 
 for %%a in (%*) do (
-    if "%%a" == "-h" goto help
+    if "%%a" == "-h" goto nocall
+    if "%%a" == "--help" goto nocall
+    if "%%a" == "-d" goto nocall
+    if "%%a" == "--debug" goto nocall
 )
 
 C:\Python27\python.exe %~dp0\uniquepath.py %* > %TMP%\uniquepath_helper.bat
 call %TMP%\uniquepath_helper.bat
 goto end
 
-:help
-uniquepath.py -h 
+:nocall
+C:\Python27\python.exe %~dp0\uniquepath.py %*
 
 :end
 exit /B %ERRORLEVEL%
