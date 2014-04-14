a37f6e06dd67509defc2491906df7bf684a2de30
elmotec <jlec@gmx.com>
2014-04-14 10:59:18 -0400

    Updated copyright notice.

--- uniquepath.py
+++ uniquepath.py
@@ -3,7 +3,7 @@
 
 """Removes duplicates in a PATH-like environment variable."""
 
-# Copyright (c) 2012 Jérôme Lecomte
+# Copyright (c) 2012-2014 Jérôme Lecomte
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
